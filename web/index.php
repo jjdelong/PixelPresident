5f945a357f74571d520f2ee299efafe222c4260a
[Josh] <48997663+jjdelong@users.noreply.github.com>
2020-11-10 11:16:28 -0600

    added nav changes and placeholder game pages

--- web/index.php
+++ web/index.php
@@ -29,36 +29,7 @@
     </div>
   </div>
 
-  <div class="main main-raised">
-    <div class="container">
-      <div id="images">
-      <div class="space-50"></div>
-        <div class="title">
-          <h2>Images</h2>
-        </div>
-        <br>
-          <div class="row">
-            <div class="col-sm-2 ml-auto">
-              <a href=""><img src="./assets/img/placeholder clear.png" alt="Game 1" class="img-raised rounded img-fluid"></a>
-              <h4 class="text-center"><a href="">Game 1</a></h4>
-            </div>
-            <div class="col-sm-2 ml-auto">
-              <a href=""><img src="./assets/img/placeholder clear.png" alt="Game 2" class="img-raised rounded img-fluid"></a>
-              <h4 class="text-center"><a href="">Game 2</a></h4>
-            </div>
-            <div class="col-sm-2 ml-auto">
-              <a href=""><img src="./assets/img/placeholder clear.png" alt="Game 3" class="img-raised rounded img-fluid"></a>
-              <h4 class="text-center"><a href="">Game 3</a></h4>
-            </div>
-            <div class="col-sm-2 ml-auto">
-              <a href=""><img src="./assets/img/placeholder clear.png" alt="Game 4" class="img-raised rounded img-fluid"></a>
-              <h4 class="text-center"><a href="">Game 4</a></h4>
-            </div>
-          </div>
-      </div>
-      <div class="space-100"></div>
-    </div>
-  </div>
+  <?php include('game-links.php'); ?>
  
   <?php include('footer.php'); ?>
 
